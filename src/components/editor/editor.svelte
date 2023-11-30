35f36e54c99116d7be0bfa9bffd6d850b0bf5954
RATIU5 <jcmemmott20@outlook.com>
2023-11-30 15:09:40 -0700

    edit: backtracked to adding event listener

---
+++ src/components/editor/editor.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let canvas: HTMLCanvasElement;
+
+	$effect(() => {
+		canvas.addEventListener('contextmenu', (e) => {
+			e.preventDefault();
+		});
+	});
+</script>
+
+<canvas bind:this={canvas} class="w-full h-full"></canvas>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,5 @@
 <script>
-	import ContextMenuItem from '../components/editor/context-menu/context-menu-item.svelte';
-	import ContextMenu from '../components/editor/context-menu/context-menu.svelte';
-	import { IconBoxMultiple, IconGridDots, IconSquarePlus2 } from '@tabler/icons-svelte';
+	import Editor from '../components/editor/editor.svelte';
 </script>
 
-<ContextMenu>
-	<ContextMenuItem>
-		<IconSquarePlus2 slot="icon" size={18} stroke={1.5} />
-		<p class="select-none">Add new node</p>
-	</ContextMenuItem>
-	<ContextMenuItem>
-		<IconBoxMultiple slot="icon" size={18} stroke={1.5} />
-		<p class="select-none">Select all</p>
-	</ContextMenuItem>
-	<ContextMenuItem>
-		<IconGridDots slot="icon" size={18} stroke={1.5} />
-		<p class="select-none">Toggle Grid</p>
-	</ContextMenuItem>
-</ContextMenu>
+<Editor />
