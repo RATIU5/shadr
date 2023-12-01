4071fbdafbc73e303889fbb38f121aa1ae3bdf71
RATIU5 <jcmemmott20@outlook.com>
2023-12-01 08:36:00 -0700

    edit: partially working context menu

--- src/components/editor/editor.svelte
+++ src/components/editor/editor.svelte
@@ -1,11 +1,60 @@
 <script lang="ts">
-	let canvas: HTMLCanvasElement;
+	let mouseX = $state(0);
+	let mouseY = $state(0);
+	let showContextMenu = $state(false);
+	let contextMenu: HTMLElement;
+	let canvasElement: HTMLCanvasElement;
 
-	$effect(() => {
-		canvas.addEventListener('contextmenu', (e) => {
-			e.preventDefault();
-		});
-	});
+	function handleContextMenu(e: Event) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		const contextRect = contextMenu.getBoundingClientRect();
+		const canvasRect = canvasElement.getBoundingClientRect();
+		if (contextRect.width + (e as MouseEvent).clientX > canvasRect.width) {
+			mouseX = (e as MouseEvent).clientX - contextRect.width;
+		} else {
+			mouseX = (e as MouseEvent).clientX;
+		}
+		if (contextRect.height + (e as MouseEvent).clientY > canvasRect.height) {
+			mouseY = (e as MouseEvent).clientY - contextRect.height;
+		} else {
+			mouseY = (e as MouseEvent).clientY;
+		}
+
+		showContextMenu = true;
+		document.addEventListener('click', handleOuterClick);
+	}
+
+	function handleOuterClick(e: Event) {
+		let inside = (e.target! as HTMLElement).closest('#context-menu');
+		if (!inside) {
+			showContextMenu = false;
+			document.removeEventListener('click', handleOuterClick);
+		}
+	}
 </script>
 
-<canvas bind:this={canvas} class="w-full h-full"></canvas>
+<div
+	bind:this={contextMenu}
+	id="context-menu"
+	class="absolute bg-neutral-800 shadow-xl rounded-md p-1 inset-border min-w-52"
+	style="display: {showContextMenu ? 'block' : 'none'}; top:{mouseY}px; left:{mouseX}px;"
+>
+	<ul class="text-neutral-300 text-sm">
+		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
+			<p>Select All</p>
+		</li>
+		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
+			<p>Copy</p>
+		</li>
+		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
+			<p>Cut</p>
+		</li>
+		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
+			<p>Paste</p>
+		</li>
+	</ul>
+</div>
+<canvas oncontextmenu={handleContextMenu} bind:this={canvasElement} class="w-screen h-screen"
+></canvas>
