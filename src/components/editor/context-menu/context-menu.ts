0754d53a14255bafa7c5f726225f25b75420b482
RATIU5 <jcmemmott20@outlook.com>
2023-12-04 10:58:03 -0700

    edit: updated context menu to action

---
+++ src/components/editor/context-menu/context-menu.ts
@@ -0,0 +1,63 @@
+export type ContextItem = {
+	label: string;
+	action: () => void;
+	disabled?: boolean;
+};
+
+export function contextMenu(node: HTMLElement, items: ContextItem[]) {
+	const menu = document.createElement('ul');
+	menu.id = 'context-menu';
+	menu.className =
+		'absolute bg-neutral-800 shadow-xl rounded-md p-1 inset-border min-w-52 text-neutral-300 text-sm';
+	menu.style.display = 'none';
+	document.body.appendChild(menu);
+
+	function updateMenuItems() {
+		menu.innerHTML = '';
+		items.forEach((item) => {
+			const li = document.createElement('li');
+			li.className =
+				'hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2';
+			li.textContent = item.label;
+			if (!item.disabled) {
+				li.addEventListener('click', () => {
+					item.action();
+					menu.style.display = 'none';
+				});
+			}
+			menu.appendChild(li);
+		});
+	}
+
+	updateMenuItems();
+
+	function handleOuterClick(event: MouseEvent) {
+		if (!menu.contains(event.target as Node)) {
+			menu.style.display = 'none';
+			document.removeEventListener('click', handleOuterClick);
+		}
+	}
+
+	node.addEventListener('contextmenu', function (e) {
+		e.preventDefault();
+
+		const x = e.clientX;
+		const y = e.clientY;
+		menu.style.top = `${y}px`;
+		menu.style.left = `${x}px`;
+		menu.style.display = 'block';
+
+		document.addEventListener('click', handleOuterClick);
+	});
+
+	return {
+		destroy() {
+			document.body.removeChild(menu);
+			node.removeEventListener('contextmenu', handleOuterClick);
+		},
+		update(newItems: Array<ContextItem>) {
+			items = newItems;
+			updateMenuItems();
+		}
+	};
+}
--- src/components/editor/editor.svelte
+++ src/components/editor/editor.svelte
@@ -1,64 +1,12 @@
 <script lang="ts">
-	import { tick } from 'svelte';
+	import { contextMenu, type ContextItem } from './context-menu/context-menu';
 
-	let mouseX = $state(0);
-	let mouseY = $state(0);
-	let showContextMenu = $state(false);
-	let contextMenu: HTMLElement;
-	let canvasElement: HTMLCanvasElement;
-
-	async function handleContextMenu(e: Event) {
-		e.preventDefault();
-		e.stopPropagation();
-		showContextMenu = true;
-
-		await tick();
-
-		const contextRect = contextMenu.getBoundingClientRect();
-		const canvasRect = canvasElement.getBoundingClientRect();
-		if (contextRect.width + (e as MouseEvent).clientX > canvasRect.width) {
-			mouseX = (e as MouseEvent).clientX - contextRect.width;
-		} else {
-			mouseX = (e as MouseEvent).clientX;
-		}
-		if (contextRect.height + (e as MouseEvent).clientY > canvasRect.height) {
-			mouseY = (e as MouseEvent).clientY - contextRect.height;
-		} else {
-			mouseY = (e as MouseEvent).clientY;
-		}
-
-		document.addEventListener('click', handleOuterClick);
-	}
-
-	function handleOuterClick(e: Event) {
-		let inside = (e.target! as HTMLElement).closest('#context-menu');
-		if (!inside) {
-			showContextMenu = false;
-			document.removeEventListener('click', handleOuterClick);
-		}
-	}
+	const items: ContextItem[] = [
+		{ label: 'Select All', action: () => console.log('Select All') },
+		{ label: 'Copy', action: () => console.log('Copy') },
+		{ label: 'Cut', action: () => console.log('Cut') },
+		{ label: 'Paste', action: () => console.log('Paste') }
+	];
 </script>
 
-<div
-	bind:this={contextMenu}
-	id="context-menu"
-	class="absolute bg-neutral-800 shadow-xl rounded-md p-1 inset-border min-w-52"
-	style="display: {showContextMenu ? 'block' : 'none'}; top:{mouseY}px; left:{mouseX}px;"
->
-	<ul class="text-neutral-300 text-sm">
-		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
-			<p>Select All</p>
-		</li>
-		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
-			<p>Copy</p>
-		</li>
-		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
-			<p>Cut</p>
-		</li>
-		<li class="hover:bg-neutral-700 rounded cursor-pointer py-2 px-3 flex items-center gap-2">
-			<p>Paste</p>
-		</li>
-	</ul>
-</div>
-<canvas oncontextmenu={handleContextMenu} bind:this={canvasElement} class="w-screen h-screen"
-></canvas>
+<canvas use:contextMenu={items} class="w-screen h-screen"></canvas>
