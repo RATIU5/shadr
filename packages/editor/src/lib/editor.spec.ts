19595bdb92998594544ffc889327fc97dc99aa78
RATIU5 <jcmemmott20@outlook.com>
2023-12-13 09:45:24 -0700

    refactor: setup nx project with basic renderer code

--- packages/editor/src/lib/editor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { editor } from './editor';
-
-describe('editor', () => {
-  it('should work', () => {
-    expect(editor()).toEqual('editor');
-  });
-});
--- packages/editor/src/lib/editor.ts
+++ packages/editor/src/lib/editor.ts
@@ -1,3 +1,23 @@
-export function editor(): string {
-  return 'editor';
+import { IApplicationOptions } from 'pixi.js';
+import { Renderer } from './renderer/renderer';
+
+export type EditorConfig = {
+  canvas: HTMLCanvasElement;
+  pixiConfig?: Partial<Omit<IApplicationOptions, 'view' | 'width' | 'height'>>;
+};
+
+export class Editor {
+  renderer: Renderer;
+  nodes: Map<string, any>;
+  connections: Set<any>;
+
+  constructor(config: EditorConfig) {
+    this.renderer = new Renderer(config.canvas, config.pixiConfig);
+    this.nodes = new Map();
+    this.connections = new Set();
+  }
+
+  start() {
+    console.log('Editor started');
+  }
 }
