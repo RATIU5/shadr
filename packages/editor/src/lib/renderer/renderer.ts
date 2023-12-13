dd24e4642324b45daa3d1bd58db5ec186442847b
RATIU5 <jcmemmott20@outlook.com>
2023-12-13 07:48:45 -0700

    edit: renderer - not compiling

--- packages/editor/src/lib/renderer/renderer.ts
+++ packages/editor/src/lib/renderer/renderer.ts
@@ -1,8 +1,10 @@
-import { Application, IApplicationOptions } from 'pixi.js';
+import { Application, Container, IApplicationOptions, Polygon } from 'pixi.js';
 
 export class Renderer {
   canvas: HTMLCanvasElement;
   app: Application;
+  container: Container;
+
   constructor(
     canvas: HTMLCanvasElement,
     pixiConfig?: Partial<Omit<IApplicationOptions, 'view' | 'width' | 'height'>>
@@ -10,9 +12,33 @@ export class Renderer {
     this.canvas = canvas;
     this.app = new Application(
       Object.assign(
-        { view: canvas, width: canvas.width, height: canvas.height },
+        {
+          view: canvas,
+          width: canvas.clientWidth,
+          height: canvas.clientHeight,
+        },
         pixiConfig
       )
     );
+    this.container = new Container();
+    this.container.hitArea = new Polygon([
+      0,
+      0,
+      this.canvas.clientWidth,
+      0,
+      this.canvas.clientWidth,
+      this.canvas.clientHeight,
+      0,
+      this.canvas.clientHeight,
+    ]);
+    this.container.eventMode = 'static';
+    this.app.stage.addChild(this.container);
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    this.container.on('mousemove', (event) => {
+      console.log('move');
+    });
   }
 }
