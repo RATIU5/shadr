6c3f7ca4d3f4459aead7978211184d8ffe61e3fe
RATIU5 <jcmemmott20@outlook.com>
2023-12-15 11:30:45 -0700

    (editor): removed periods in doc comments

--- packages/editor/src/lib/events/event-bus.ts
+++ packages/editor/src/lib/events/event-bus.ts
@@ -5,13 +5,13 @@ export type EventListeners = {
 export type EventType = "editor:ready" | "editor:start" | "editor:mouseDown" | "editor:mouseMove" | "editor:mouseUp";
 
 /**
- * A simple event bus that allows for subscribing to and emitting events.
+ * A simple event bus that allows for subscribing to and emitting events
  */
 export class EventBus {
   #eventListeners: EventListeners;
 
   /**
-   * Initializes the event bus by creating an empty object for storing event listeners.
+   * Initializes the event bus by creating an empty object for storing event listeners
    * @constructor
    */
   constructor() {
@@ -19,9 +19,9 @@ export class EventBus {
   }
 
   /**
-   * Adds a listener for a specific event. If you need to remove the listener later, you should store a reference to the callback function.
-   * @param {EventType} event - The name of the event to emit.
-   * @param {Callback} listener - The callback function to execute when the event is emitted.
+   * Adds a listener for a specific event. If you need to remove the listener later, you should store a reference to the callback function
+   * @param {EventType} event - The name of the event to emit
+   * @param {Callback} listener - The callback function to execute when the event is emitted
    */
   on(event: EventType, listener: Callback) {
     if (!this.#eventListeners[event]) {
@@ -31,9 +31,9 @@ export class EventBus {
   }
 
   /**
-   * Removes a listener for a specific event.
-   * @param {EventType} event - The name of the event.
-   * @param {Callback} listenerToRemove - The callback function to be removed.
+   * Removes a listener for a specific event
+   * @param {EventType} event - The name of the event
+   * @param {Callback} listenerToRemove - The callback function to be removed
    */
   off(event: EventType, listenerToRemove: Callback) {
     if (!this.#eventListeners[event]) {
@@ -43,9 +43,9 @@ export class EventBus {
   }
 
   /**
-   * Emits an event to all registered listeners.
-   * @param {EventType} event - The name of the event to emit.
-   * @param {unknown} data - The data to pass to each listener's callback function.
+   * Emits an event to all registered listeners
+   * @param {EventType} event - The name of the event to emit
+   * @param {unknown} data - The data to pass to each listener's callback function
    */
   emit<T = unknown>(event: EventType, data?: T) {
     if (!this.#eventListeners[event]) {
