20de7772ed0f7398471dafd4b4ecd7f084baf39b
RATIU5 <jcmemmott20@outlook.com>
2023-12-15 08:15:20 -0700

    (editor): change the renderer type and add the stage container

--- packages/editor/src/lib/editor.ts
+++ packages/editor/src/lib/editor.ts
@@ -1,20 +1,23 @@
-import { Renderer } from "pixi.js";
+import { Container, ICanvas, IRenderer, autoDetectRenderer } from "pixi.js";
 import { EventBus } from "./events/event-bus";
 
 export type EditorConfig = {
   canvas: HTMLCanvasElement;
 };
 
-export class Editor {
-  renderer: Renderer;
+export class Editor<VIEW extends ICanvas = ICanvas> {
+  renderer: IRenderer<VIEW>;
   eventBus: EventBus;
+  stage: Container;
 
   constructor(config: EditorConfig) {
-    this.renderer = new Renderer({
+    // Configure Pixi.js rendering engine
+    this.renderer = autoDetectRenderer<VIEW>({
       view: config.canvas,
       width: config.canvas.clientWidth,
       height: config.canvas.clientHeight,
     });
+    this.stage = new Container();
 
     this.eventBus = new EventBus();
 
