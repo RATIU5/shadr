9eac9b7e115c53404999ea906c90109700589b32
RATIU5 <jcmemmott20@outlook.com>
2023-12-15 12:44:07 -0700

    (editor): update the interactions and events to drag the grid

--- packages/editor/src/lib/editor.ts
+++ packages/editor/src/lib/editor.ts
@@ -8,8 +8,17 @@ export type EditorConfig = {
 };
 
 export type InteractionState = {
-  mouseDown: boolean;
+  dragDown: boolean;
+  zoomFactor: number;
   mousePosition: {
+    x: number;
+    y: number;
+  };
+  dragStart: {
+    x: number;
+    y: number;
+  };
+  dragOffset: {
     x: number;
     y: number;
   };
@@ -17,22 +26,32 @@ export type InteractionState = {
 
 export type EditorEvents = {
   "editor:ready": undefined;
-  "editor:mouseDown": FederatedPointerEvent;
-  "editor:mouseMove": FederatedPointerEvent;
-  "editor:mouseUp": FederatedPointerEvent;
+  "editor:pointerDown": FederatedPointerEvent;
+  "editor:pointerMove": FederatedPointerEvent;
+  "editor:pointerUp": FederatedPointerEvent;
 };
 
 export class Editor<VIEW extends ICanvas = ICanvas> {
   renderer: IRenderer<VIEW>;
   eventBus: EventBus<EditorEvents>;
   stage: Container;
+  grid: Grid;
   interactionState: State<InteractionState>;
 
   constructor(config: EditorConfig) {
     this.eventBus = new EventBus();
     this.interactionState = new State({
-      mouseDown: false,
+      dragDown: false,
+      zoomFactor: 1,
       mousePosition: {
+        x: 0,
+        y: 0,
+      },
+      dragStart: {
+        x: 0,
+        y: 0,
+      },
+      dragOffset: {
         x: 0,
         y: 0,
       },
@@ -54,30 +73,60 @@ export class Editor<VIEW extends ICanvas = ICanvas> {
     this.setupEventListeners();
 
     // Setup grid background and add to stage
-    const grid = new Grid(config.canvas.clientWidth, config.canvas.clientHeight);
-    this.stage.addChild(grid.getMesh());
+    this.grid = new Grid(config.canvas.clientWidth, config.canvas.clientHeight);
+    this.stage.addChild(this.grid.getMesh());
 
     this.eventBus.emit("editor:ready");
   }
 
   setupEventListeners() {
+    // Setup event emitter for pointer events
     this.stage.on("pointerdown", (event) => {
-      this.eventBus.emit("editor:mouseDown", event);
+      this.eventBus.emit("editor:pointerDown", event);
     });
     this.stage.on("pointermove", (event) => {
-      this.eventBus.emit("editor:mouseMove", event);
+      this.eventBus.emit("editor:pointerMove", event);
     });
     this.stage.on("pointerup", (event) => {
-      this.eventBus.emit("editor:mouseUp", event);
+      this.eventBus.emit("editor:pointerUp", event);
     });
 
-    this.eventBus.on("editor:mouseDown", (event) => {
-      this.interactionState.set("mouseDown", true);
-      this.interactionState.set("mousePosition", {
-        x: event?.clientX ?? 0,
-        y: event?.clientY ?? 0,
-      });
-      console.log("mouseDown", this.interactionState.get("mousePosition"));
+    // Setup actions for emitted events
+    this.eventBus.on("editor:pointerDown", (event) => {
+      if (event?.button === 1) {
+        this.interactionState.set("dragDown", true);
+        this.interactionState.set("dragStart", {
+          x: event?.clientX ?? 0,
+          y: event?.clientY ?? 0,
+        });
+      }
+    });
+    this.eventBus.on("editor:pointerUp", () => {
+      this.interactionState.set("dragDown", false);
+    });
+    this.eventBus.on("editor:pointerMove", (event) => {
+      if (this.interactionState.get("dragDown")) {
+        const deltaX = (event?.clientX ?? 0) - this.interactionState.get("dragStart").x;
+        const deltaY = (event?.clientY ?? 0) - this.interactionState.get("dragStart").y;
+
+        this.interactionState.set("dragOffset", {
+          x: this.interactionState.get("dragOffset").x + deltaX * this.interactionState.get("zoomFactor"),
+          y: this.interactionState.get("dragOffset").y + deltaY * this.interactionState.get("zoomFactor"),
+        });
+
+        this.grid.setUniform("u_dragOffset", [
+          this.interactionState.get("dragOffset").x,
+          this.interactionState.get("dragOffset").y,
+        ]);
+
+        this.interactionState.set("dragStart", {
+          x: event?.clientX ?? 0,
+          y: event?.clientY ?? 0,
+        });
+      }
+
+      // Render the stage with the new positions
+      this.renderer.render(this.stage);
     });
   }
 
