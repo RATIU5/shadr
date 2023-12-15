e45ab3423afea6a99a30c1990048b392492a814e
RATIU5 <jcmemmott20@outlook.com>
2023-12-14 18:59:33 -0700

    (event-bus): update the event listener object to js private #

--- packages/editor/src/lib/events/event-bus.ts
+++ packages/editor/src/lib/events/event-bus.ts
@@ -7,26 +7,26 @@ export type EventListeners = {
  * A simple event bus that allows for subscribing to and emitting events.
  */
 export class EventBus {
-  private eventListeners: EventListeners;
+  #eventListeners: EventListeners;
 
   /**
    * Initializes the event bus by creating an empty object for storing event listeners.
    * @constructor
    */
   constructor() {
-    this.eventListeners = {};
+    this.#eventListeners = {};
   }
 
   /**
-   * Emits an event to all registered listeners.
+   * Adds a listener for a specific event.
    * @param {string} event - The name of the event to emit.
    * @param {Callback} listener - The callback function to execute when the event is emitted.
    */
   on(event: string, listener: Callback) {
-    if (!this.eventListeners[event]) {
-      this.eventListeners[event] = [];
+    if (!this.#eventListeners[event]) {
+      this.#eventListeners[event] = [];
     }
-    this.eventListeners[event].push(listener);
+    this.#eventListeners[event].push(listener);
   }
 
   /**
@@ -35,10 +35,10 @@ export class EventBus {
    * @param {Callback} listenerToRemove - The callback function to be removed.
    */
   off(event: string, listenerToRemove: Callback) {
-    if (!this.eventListeners[event]) {
+    if (!this.#eventListeners[event]) {
       return;
     }
-    this.eventListeners[event] = this.eventListeners[event].filter((listener) => listener !== listenerToRemove);
+    this.#eventListeners[event] = this.#eventListeners[event].filter((listener) => listener !== listenerToRemove);
   }
 
   /**
@@ -47,10 +47,10 @@ export class EventBus {
    * @param {any} data - The data to pass to each listener's callback function.
    */
   emit(event: string, data: unknown) {
-    if (!this.eventListeners[event]) {
+    if (!this.#eventListeners[event]) {
       return;
     }
-    for (const listener of this.eventListeners[event]) {
+    for (const listener of this.#eventListeners[event]) {
       listener(data);
     }
   }
