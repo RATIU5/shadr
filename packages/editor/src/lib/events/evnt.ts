d34eaf8a5606588f582ce23f8d042ae68869ae1c
RATIU5 <jcmemmott20@outlook.com>
2023-12-22 20:01:18 -0700

    (editor): edit evnt class, getting there

--- packages/editor/src/lib/events/evnt.ts
+++ packages/editor/src/lib/events/evnt.ts
@@ -11,65 +11,30 @@ export class Evnt {
   }
 
   public bind(node: Node, eventType: string, eventName: string, condition?: ConditionFunction) {
-    // Create a new inner map for the node if it doesn't exist
     if (!this.nodeEventMap.has(node)) {
       this.nodeEventMap.set(node, new Map());
     }
 
-    // Get the inner map for the node
-    const nodeEventMap = this.nodeEventMap.get(node);
+    const nodeMap = this.nodeEventMap.get(node);
 
-    // Create a new array for the event type if it doesn't exist
-    if (!nodeEventMap?.has(eventType)) {
-      nodeEventMap?.set(eventType, []);
+    if (nodeMap && !nodeMap.has(eventType)) {
+      nodeMap.set(eventType, []);
+      node.addEventListener(eventType, (e) => this.callbackHandler(eventName, e));
     }
 
-    // Get the array for the event type
-    const eventArray = nodeEventMap?.get(eventType);
+    const event = nodeMap?.get(eventType);
 
-    // Check if the event name already exists
-    if (eventArray?.some((event) => event.eventName === eventName)) {
-      return console.warn(`Event ${eventName} already exists for node ${node} and event type ${eventType}`);
+    if (event && !event.some((e) => e.eventName === eventName)) {
+      event.push({
+        condition,
+        eventName,
+      });
     }
+  }
 
-    // Add the event to the array
-    eventArray?.push({ condition, eventName });
-
-    // Add the event listener
-    node.addEventListener(eventType, (e) => {
-      // Get the array of events for the node and event type
-      const events = this.nodeEventMap.get(node)?.get(eventType);
-
-      // Emit all events that match the condition
-      for (const { eventName, condition } of events ?? []) {
-        if (!condition || condition(e)) {
-          this.emit(eventName, e);
-        }
-      }
-    });
-
+  private callbackHandler(eventName: string, event: Event) {
     console.log(this.nodeEventMap);
   }
 
-  public emit(eventName: string, e: Event) {
-    // Get the array of listeners for the event name
-    const listeners = this.listeners.get(eventName);
-
-    // Call all listeners
-    for (const listener of listeners ?? []) {
-      listener(e);
-    }
-  }
-
-  public on(eventName: string, listener: EventListenerCallback) {
-    // Check if the event name already exists
-    if (!this.listeners.has(eventName)) {
-      this.listeners.set(eventName, []);
-    }
-    // Get the array of listeners for the event name
-    const listeners = this.listeners.get(eventName);
-
-    // Add the listener to the array
-    listeners?.push(listener);
-  }
+  public on(eventName: string, callback: CallableFunction) {}
 }
