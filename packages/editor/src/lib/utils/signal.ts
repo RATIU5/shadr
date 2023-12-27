8ff2f04b99e92237fc889213677d54f6704783e5
RATIU5 <jcmemmott20@outlook.com>
2023-12-27 11:46:43 -0700

    (editor): add basic signal for variable change effects

---
+++ packages/editor/src/lib/utils/signal.ts
@@ -0,0 +1,74 @@
+/**
+ * Creates a signal with getter and setter functions to manage and observe state changes.
+ * @template T The type of the value managed by the signal.
+ * @param initialValue The initial value of the signal.
+ * @returns An object containing methods to get, set, subscribe, unsubscribe, and unsubscribe all listeners.
+ *
+ * @example **Basic Usage**
+ * ```ts
+ * const signal = createSignal(0);
+ *
+ * signal.sub((value) => {
+ *  console.log(value);
+ * });
+ *
+ * signal.set(1);
+ * signal.set(2);
+ * ```
+ * The above example will log the following:
+ * ```
+ * 1
+ * 2
+ * ```
+ */
+export function createSignal<T>(initialValue: T) {
+  let _value = initialValue;
+  let _listeners: ((value: T) => void)[] = [];
+
+  /**
+   * Gets the current value of the signal.
+   * @returns The current value.
+   */
+  function get() {
+    return _value;
+  }
+
+  /**
+   * Sets a new value for the signal and notifies all listeners.
+   * @param newValue The new value to be set.
+   */
+  function set(newValue: T) {
+    _value = newValue;
+
+    for (const listener of _listeners) {
+      listener(newValue);
+    }
+  }
+
+  /**
+   * Subscribes a new listener function to the signal.
+   * @param func The function to be called when the signal value changes.
+   */
+  function sub(func: (val: T) => void) {
+    if (func) {
+      _listeners.push(func);
+    }
+  }
+
+  /**
+   * Unsubscribes a listener function from the signal.
+   * @param func The listener function to be removed.
+   */
+  function unsub(func: (val: T) => void) {
+    _listeners = _listeners.filter((listener) => listener !== func);
+  }
+
+  /**
+   * Unsubscribes all listener functions from the signal.
+   */
+  function unsubAll() {
+    _listeners = [];
+  }
+
+  return { get, set, sub, unsub, unsubAll };
+}
