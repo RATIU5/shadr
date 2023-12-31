67a5439092bf01ebe6de230825eb86fdc93b5b50
RATIU5 <jcmemmott20@outlook.com>
2023-12-30 19:46:00 -0700

    (web): add context menu

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,6 +1,57 @@
 import { Editor } from "@shadr/editor/src";
+import { initializeContextComponent } from "./components/context-menu";
 
+const nodeEditor = document.getElementById("node-editor") as HTMLCanvasElement;
 const editor = new Editor({
-  canvas: document.getElementById("node-editor") as HTMLCanvasElement,
+  canvas: nodeEditor,
 });
+
+initializeContextComponent(nodeEditor, [
+  {
+    type: "item",
+    label: "Add new node",
+    items: [
+      {
+        type: "item",
+        label: "Input&nbsp;node",
+        action: () => console.log("i"),
+      },
+      {
+        type: "item",
+        label: "Output&nbsp;node",
+        action: () => console.log("o"),
+      },
+    ],
+  },
+  {
+    type: "item",
+    label: "Select all",
+    action: () => console.log("a"),
+    disabled: true,
+  },
+  { type: "separator" },
+  {
+    type: "item",
+    label: "Zoom in",
+    action: () => {
+      editor.setZoom(editor.getZoom() - 0.5);
+    },
+  },
+  {
+    type: "item",
+    label: "Zoom out",
+    action: () => {
+      editor.setZoom(editor.getZoom() + 0.5);
+    },
+  },
+  {
+    type: "item",
+    label: "Reset view",
+    action: () => {
+      editor.setZoom(1);
+      editor.setOffset(0, 0);
+    },
+  },
+]);
+
 editor.start();
