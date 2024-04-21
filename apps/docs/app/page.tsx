3ecf5952093cc48bca1c50dcd530d83525c8c97a
RATIU5 <jcmemmott20@outlook.com>
2024-04-20 20:23:08 -0600

    reset turborepo

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
 
 function Gradient({
   conic,
@@ -46,8 +43,7 @@ const LINKS = [
   {
     title: "Deploy",
     href: "https://vercel.com/new",
-    description:
-      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
+    description: "Instantly deploy your Turborepo to a shareable URL with Vercel.",
   },
 ];
 
@@ -55,10 +51,7 @@ export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          examples/basic&nbsp;
-          <Code className={styles.code}>docs</Code>
-        </p>
+        <p>examples/basic&nbsp;</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"
@@ -66,47 +59,23 @@ export default function Page(): JSX.Element {
             target="_blank"
           >
             By{" "}
-            <Image
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              height={24}
-              priority
-              src="/vercel.svg"
-              width={100}
-            />
+            <Image alt="Vercel Logo" className={styles.vercelLogo} height={24} priority src="/vercel.svg" width={100} />
           </a>
         </div>
       </div>
-
-      <Button appName="docs" className={styles.button}>
-        Click me!
-      </Button>
 
       <div className={styles.hero}>
         <div className={styles.heroContent}>
           <div className={styles.logos}>
             <div className={styles.circles}>
-              <Image
-                alt="Turborepo"
-                height={614}
-                src="circles.svg"
-                width={614}
-                style={{ pointerEvents: "none" }}
-              />
+              <Image alt="Turborepo" height={614} src="circles.svg" width={614} style={{pointerEvents: "none"}} />
             </div>
             <div className={styles.logoGradientContainer}>
               <Gradient className={styles.logoGradient} conic small />
             </div>
 
             <div className={styles.logo}>
-              <Image
-                alt=""
-                height={120}
-                priority
-                src="turborepo.svg"
-                width={120}
-                style={{ pointerEvents: "none" }}
-              />
+              <Image alt="" height={120} priority src="turborepo.svg" width={120} style={{pointerEvents: "none"}} />
             </div>
           </div>
           <Gradient className={styles.backgroundGradient} conic />
@@ -130,14 +99,6 @@ export default function Page(): JSX.Element {
             </svg>
           </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        {LINKS.map(({ title, href, description }) => (
-          <Card className={styles.card} href={href} key={title} title={title}>
-            {description}
-          </Card>
-        ))}
       </div>
     </main>
   );
